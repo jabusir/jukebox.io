157ed6b2d75cdad6810d4008b2933225c2c0a5fb
jabusir <jabusir@gmail.com>
2018-08-28 23:14:19 -0700

    for the sake of god commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,9 @@
 import React from 'react';
-<<<<<<< HEAD
 import Playlist from './Playlist'
 
 export default class Dashboard extends React.Component{
     componentDidMount(){
-        this.props.history.push('/login');
+        this.props.history.push('/login')
     }
     render(){
         return(
@@ -12,19 +11,3 @@ export default class Dashboard extends React.Component{
         );
     }
 }
-=======
-import Playlist from './Playlist';
-import AppRouter from '../routes/AppRouter';
-
-export default class Dashboard extends React.Component{
-    componentDidMount(){
-    }
-    render(){
-        return(
-            <div>
-                <AppRouter />
-             </div>
-        );
-    }
-}
->>>>>>> parent of 01128b9... dummy login successful added
